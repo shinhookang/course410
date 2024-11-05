82c8f3a07e970ed5b912b51aa7ff5255c7ed16a3
shinhoo <brock.kang@gmail.com>
2024-11-05 16:53:59 +0900

    update

--- 08-MPI/ex1/ex1-helloworld.c
+++ 08-MPI/ex1/ex1-helloworld.c
@@ -5,13 +5,10 @@ int main(int argc, char** argv) {
     // Initialize the MPI environment
     MPI_Init(&argc, &argv);
 
-    // Get the rank (ID) of the current process
     int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the total number of processes
     int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Get the rank (ID) of the current process
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Get the total number of processes
 
     // Print a "Hello, World!" message from each process
     printf("Hello, World! from process %d of %d\n", world_rank, world_size);
