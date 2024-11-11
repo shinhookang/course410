b22e15f742abb594dceadbf11f4f8431fb09d6a4
shinhoo <brock.kang@gmail.com>
2024-11-12 08:29:11 +0900

    update

--- 08-MPI/ex6/funnel.c
+++ 08-MPI/ex6/funnel.c
@@ -12,11 +12,18 @@ int main(int argc, char *argv[]) {
     MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // Master thread makes all MPI calls 
-    #pragma omp parallel for reduction(+:count)
-    for (int i = 0; i < 100; i++) {
-      count++;
+    // Master thread makes all MPI calls
+    #pragma omp parallel reduction(+:count)
+    {
+      #pragma omp for 
+      for (int i = 0; i < 100; i++) {
+        count++;
+      }
     }
+    //#pragma omp parallel for reduction(+:count)
+    //for (int i = 0; i < 100; i++) {
+    //  count++;
+    //}
             
     printf("Process %d counts: %d\n", rank, count);
 
