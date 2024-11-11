3bdd285a99dd07227d614f348cc30d7db3078dee
shinhoo <brock.kang@gmail.com>
2024-11-11 22:57:36 +0900

    update

--- 08-MPI/ex6/multiple.c
+++ 08-MPI/ex6/multiple.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[]) {
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
+
     #pragma omp parallel 
     {
         int tid = omp_get_thread_num();
@@ -31,11 +32,11 @@ int main(int argc, char *argv[]) {
         if (rank == 0) {
             // Thread on process 0 sends a data to the corresponding thread on process 1
             MPI_Send(&data, 1, MPI_INT, 1, tid, MPI_COMM_WORLD);
-            printf("Process 0, Thread %d sent data: %d to Process 1, Thread %d\n", tid, data, tid);
+            printf("(Proc 0, Thread %d) sent data: %d to (Proc 1, Thread %d)\n", tid, data, tid);
         } else if (rank == 1) {
             // Thread on process 1 receives a data from the corresponding thread on process 0
             MPI_Recv(&data, 1, MPI_INT, 0, tid, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Process 1, Thread %d received data: %d from Process 0, Thread %d\n", tid, data, tid);
+            printf("(Proc 1, Thread %d) recv data: %d from (Proc 0, Thread %d)\n", tid, data, tid);
         }
     }
 
